19b80132b1eff74151786ae0cc27d5bf44984c3f
Srihari Rao <harirao3@gmail.com>
2013-11-28 15:59:19 -0500

    connecting to mongo

--- app/mongoLoad.js
+++ app/mongoLoad.js
@@ -1,1 +1,10 @@
 var require = require("http");
+
+var MongoClient = require('mongodb').MongoClient;
+
+//Connect to the db
+MongoClient.connect("mongodb://localhost:27017/collegeData",function (err,db){
+    if(!err){
+        console.log("We are connected");
+    }
+});
