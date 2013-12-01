8ce36835a9094d6b1867278cf81ec727027dbefa
Srihari Rao <harirao3@gmail.com>
2013-11-30 19:44:37 -0500

    question3

--- routes/index.js
+++ routes/index.js
@@ -100,7 +100,48 @@ exports.question2 = function() {
     };
 };
 
+exports.question3 = function() {
+    function compare(a,b) {
+        if (a.F1A18 > b.F1A18)
+            return -1;
+        if (a.F1A18 < b.F1A18)
+            return 1;
+        return 0;
+    }
+    return function(req, res) {
+        var opts1 = {sort:{F1A18:-1}};
+        var flds1 = {UNITID:1,F1A18:1};
+        var flds2 = {INSTNM:1};
+        var results = [];
+        coll = collegeDB.collection("univs")
+        coll.find( {rowType:"FIN"}, flds1, opts1).toArray(
+            function (err, docs) {
+                docs.forEach(function(oneDoc){
+                    console.log(oneDoc._id,oneDoc.F1A18);
+                    if(oneDoc.F1A18!=""){
+                        coll.find({rowType:"GEN",UNITID:oneDoc.UNITID},flds2).toArray(
+                            function(err,doc){
+                                if (err) console.log(err);
+                                var univName = doc[0].INSTNM;
+                                var oneResult = {UNITID:oneDoc.UNITID, F1A18:oneDoc.F1A18, INSTNM:univName};
+                                //console.log(results);
+                                results.push(oneResult);
+                                if(results.length==20){
+                                    results.sort(compare);
+                                    //console.log(results);
+                                    res.render('question3',{"question3":results});
+                                }
+                            }
+                        )
+                    }
 
+                })
+            }
+        );
+
+
+    };
+};
 exports.univTest = function() {
     return function(req, res) {
        coll = collegeDB.collection("univs")
