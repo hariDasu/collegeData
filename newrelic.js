c1595a862933bba7fa9b6379d1c5804cac0eb3ec
kinghenry8 <harirao3@gmail.com>
2013-12-10 16:12:45 +0000

    installed newrelic--working, installed mongoMMS --not working without replicasets or sharding

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['collegeData'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '702e4dd430141a71128a2f8ed2b0faef5f4a4e61',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
